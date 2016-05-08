7c37ec09ffd79e2530c8cbe59e896f002dfb3329
Matevž <mc8873@student.uni-lj.si>
2016-05-08 09:58:17 +0000

    Zaključek dela na III. nalogi

--- eracun.js
+++ eracun.js
@@ -133,7 +133,17 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      
+      if (napaka) {
+        callback(false);
+      }
+      else {
+        for (var i=0; i<vrstice.length; i++) {
+          vrstice[i].stopnja = davcnaStopnja((vrstice[i].opisArtikla.split(' (')[1]).split(')')[0], vrstice[i].zanr);
+        }
+        callback(vrstice);
+      }
+      
     })
 }
 
@@ -142,13 +152,46 @@ var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
             WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
     function(napaka, vrstice) {
-      console.log(vrstice);
+      if(napaka){
+        callback(false);
+      }
+      else {
+        callback(vrstice);
+      }
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
+  var form = new formidable.IncomingForm();
+ 
+  form.parse(zahteva, function (napaka1, polja, datoteke) {
+  var racun=polja.seznamRacunov;
+    
+    strankaIzRacuna(racun, function(podatki) {
+      pesmiIzRacuna(racun, function(pesmi) {
+        
+        if(!pesmi){
+          odgovor.sendStatus(500);
+        }
+        else if (pesmi.length == 0){
+          odgovor.send("<p>V košarici nimate nobene pesmi, \
+          zato računa ni mogoče pripraviti!</p>");
+        }else {
+          odgovor.setHeader('content-type', 'text/xml');
+          odgovor.render('eslog', {
+          vizualiziraj: true,
+          postavkeRacuna: pesmi, narocnik: podatki
+          })  
+        }
+        
+      })
+      
+    })
+    
+    
+  })
+  
 })
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
